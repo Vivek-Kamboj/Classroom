798ead1a04515099563d90a0906c2365c2bd6cc9
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2021-08-30 21:26:08 +0530

    Added response for assignment API

--- Backend/routes/assignment.js
+++ Backend/routes/assignment.js
@@ -10,6 +10,8 @@ router.get("/:assignmentId", mw.auth.verify, ctrl.assignment.get);
 // Get Assignments list for a subject
 router.get("/", mw.auth.verify, ctrl.assignment.assList);
 // Post Assignment response
+router.post("/:assignmentId/response", mw.auth.verify, ctrl.assignment.response);
+// Give marks
 // Update Assignment
 router.put("/:assignmentId", mw.auth.verify, ctrl.assignment.update);
 // Delete Assignment
