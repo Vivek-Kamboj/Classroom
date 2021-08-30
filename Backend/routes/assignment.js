6dee0868ec1773bbfdcf7d2750b362b58e8565b1
Vivek-Kamboj <vivekkamboj4555@gmail.com>
2021-08-30 17:07:25 +0530

    Added Join subject API

--- Backend/routes/assignment.js
+++ Backend/routes/assignment.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../controllers");
+const mw = require("../middleware");
 
 // Create Assignment
-router.post("/", ctrl.assignment.create);
+router.post("/", mw.auth.verify, ctrl.assignment.create);
 // Get Assignment details
-router.get("/:assignmentId", ctrl.assignment.get);
+router.get("/:assignmentId", mw.auth.verify, ctrl.assignment.get);
 // Get Assignments list for a subject
 // Post Assignment response
 // Update Assignment
-router.put("/:assignmentId", ctrl.assignment.update);
+router.put("/:assignmentId", mw.auth.verify, ctrl.assignment.update);
 // Delete Assignment
-router.delete("/:assignmentId", ctrl.assignment.deleteAssignment);
+router.delete("/:assignmentId", mw.auth.verify, ctrl.assignment.deleteAssignment);
 
 module.exports = router;
